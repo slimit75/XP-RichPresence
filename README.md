84f8479f1e78d2c521632d4756597de21fb40a95
Speed_Limit75 <limitspeed75@gmail.com>
2019-05-19 13:06:30 -0400

    Build 190503
    Alpha 0.2a
    + SPD DISAGREE replaces speed if a diffrence in indicated speed is shown
    + More aircraft supported

--- README.md
+++ README.md
@@ -5,11 +5,16 @@ Discord Rich Presence for X-Plane 11.
 - FlyWithLua NG v2.7.17 and up (https://forums.x-plane.org/index.php?/files/file/38445-flywithlua-ng-next-generation-edition-for-x-plane-11-win-lin-mac/)
 ## How to install
 - Copy all files and paste them into X-Plane 11\Resources\plugins\FlyWithLua\
+## Changelog
+### v0.2
+- SPD DISAGREE replaces speed if a diffrence in indicated speed is shown
+- More aircraft supported
 ## List of aircraft types with images
-- Embraer E-Jets (E170, E195 only currently)
-### Comming Soon:
+- Embraer E-Jets (E175, E190 now supported)
 - Robin DR401
+- Cessna 172
 - Boeing 737NG
+### Comming Soon:
 - Boeing 757
 - Airbus A320
 - MD-88
--- Scripts/XP-RichPresence.lua
+++ Scripts/XP-RichPresence.lua
@@ -7,33 +7,54 @@ image = "na"
 function refresh()
     dataref("acf", "sim/aircraft/view/acf_descrip", "readable")
     dataref("acfIcao", "sim/aircraft/view/acf_ICAO", "readable")
-    dataref("knots", "sim/flightmodel/position/indicated_airspeed", "readable")
+    dataref("kts", "sim/flightmodel/position/indicated_airspeed", "readable")
+    dataref("kts2", "sim/flightmodel/position/indicated_airspeed2", "readable")
     dataref("altitude", "sim/flightmodel/position/elevation", "readable")
     altitude = altitude * 3.28084
 
-    if (acfIcao == "E170")
-    then
+    if (kts ~= kts2) then
+        kts = "SPD DISAGREE"
+    else
+        kts = math.floor(kts) .. "kts"
+    end
+
+    if (acfIcao == "E170") then
         aircraft = "Embraer E170"
         image = "e-jets"
-    elseif (acfIcao == "E195")
-    then
+    elseif (acfIcao == "E175") then
+        aircraft = "Embrear E175"
+        image = "e-jets"
+    elseif (acfIcao == "E190") then
+        aircraft = "Embrear E190"
+        image = "e-jets"
+    elseif (acfIcao == "E195") then
         aircraft = "Embrear E195"
         image = "e-jets"
-    elseif (acfIcao == "DR40")
-    then
+    elseif (acfIcao == "DR40") then
         aircraft = "Robin DR401"
-    else
-        aircraft = acf
+        image = "dr40"
+    elseif (acfIcao == "C172") then
+        aircraft = "Cessna 172 Skyhawk"
+        image = "c172"
+    elseif (acfIcao == "B737") then
+        aircraft = "Boeing 737-700"
+        image = "737ng"
+    elseif (acfIcao == "B738") then
+        aircraft = "Boeing 737-800"
+        image = "737ng"
+    elseif (acfIcao == "B739") then
+        aircraft = "Boeing 737-900ER"
+        image = "737ng"
     end
 
     discordRPC.updatePresence({
-        ["state"] = math.floor(altitude) .. "ft, " .. math.floor(knots) .. "kts",
+        ["state"] = math.floor(altitude) .. "ft, " .. kts,
         ["details"] = aircraft,
         ["startTimestamp"] = startTime,
         ["largeImageKey"] = image,
         ["largeImageText"] = acfIcao,
         ["smallImageKey"] = "xp",
-        ["smallImageText"] = "XP-RichPresence v0.1.1a"
+        ["smallImageText"] = "v0.2a (Unreleased)"
     })
 end
 
