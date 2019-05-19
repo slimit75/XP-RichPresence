af236fd70d5b8bcdbd31ef87f4a51948f0542116
Speed_Limit75 <limitspeed75@gmail.com>
2019-05-19 11:32:54 -0400

    Build 190502
    Alpha 0.1.1a
    - Fixes fatal error
    - Aircraft name fix for Aerobask Robin DR401

--- Scripts/XP-RichPresence.lua
+++ Scripts/XP-RichPresence.lua
@@ -1,8 +1,8 @@
-local discordRPC = require "discordRPC"
+discordRPC = require "discordRPC"
 
-local startTime = os.time()
-local aircraft = "N/A"
-local image = "na"
+startTime = os.time()
+aircraft = "N/A"
+image = "na"
 
 function refresh()
     dataref("acf", "sim/aircraft/view/acf_descrip", "readable")
@@ -11,28 +11,38 @@ function refresh()
     dataref("altitude", "sim/flightmodel/position/elevation", "readable")
     altitude = altitude * 3.28084
 
-    if acfIcao == "E170" then
+    if (acfIcao == "E170")
+    then
         aircraft = "Embraer E170"
         image = "e-jets"
-    elseif acfIcao == "E195" then
+    elseif (acfIcao == "E195")
+    then
         aircraft = "Embrear E195"
         image = "e-jets"
+    elseif (acfIcao == "DR40")
+    then
+        aircraft = "Robin DR401"
     else
         aircraft = acf
     end
 
     discordRPC.updatePresence({
-        ["state"] = math.floor(altitude) .. "ft, " .. math.floor(knots) .. "kts"
-        ["details"] = aircraft
-        ["startTimestamp"] = startTime
-        ["largeImageKey"] = image
-        ["largeImageText"] = acfIcao
-        ["smallImageKey"] = "xp"
-        ["smallImageText"] = "XP-RichPresence v0.1a"
+        ["state"] = math.floor(altitude) .. "ft, " .. math.floor(knots) .. "kts",
+        ["details"] = aircraft,
+        ["startTimestamp"] = startTime,
+        ["largeImageKey"] = image,
+        ["largeImageText"] = acfIcao,
+        ["smallImageKey"] = "xp",
+        ["smallImageText"] = "XP-RichPresence v0.1.1a"
     })
 end
 
-local function run()
+function run()
     discordRPC.initialize("579668847846752266", false, "269950")
     do_on_exit("discordRPC.clearPresence()")
     do_often("refresh()")
+end
+
+local ok = xpcall(run, function(err)
+    logMsg(err)
+end)
