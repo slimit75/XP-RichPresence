afd29bfa24ef3d0e974ac73da5c4cf6c113aaec2
slimit75 <limitspeed75@gmail.com>
2019-12-04 19:12:00 -0500

    Edit comments on XP-RP.cpp

--- XP-RP.cpp
+++ XP-RP.cpp
@@ -19,10 +19,7 @@
 #error This is made to be compiled against the XPLM300 SDK
 #endif
 
-// An opaque handle to the window we will create
 static XPLMWindowID	g_window;
-
-// Callbacks we will register when we create our window
 void				draw_hello_world(XPLMWindowID in_window_id, void* in_refcon);
 int					dummy_mouse_handler(XPLMWindowID in_window_id, int x, int y, int is_down, void* in_refcon) { return 0; }
 XPLMCursorStatus	dummy_cursor_status_handler(XPLMWindowID in_window_id, int x, int y, void* in_refcon) { return xplm_CursorDefault; }
@@ -42,8 +39,6 @@ PLUGIN_API int XPluginStart(
 	params.structSize = sizeof(params);
 	params.visible = 1;
 	params.drawWindowFunc = draw_hello_world;
-	// Note on "dummy" handlers:
-	// Even if we don't want to handle these events, we have to register a "do-nothing" callback for them
 	params.handleMouseClickFunc = dummy_mouse_handler;
 	params.handleRightClickFunc = dummy_mouse_handler;
 	params.handleMouseWheelFunc = dummy_wheel_handler;
@@ -52,10 +47,6 @@ PLUGIN_API int XPluginStart(
 	params.refcon = NULL;
 	params.layer = xplm_WindowLayerFloatingWindows;
 	params.decorateAsFloatingWindow = xplm_WindowDecorationRoundRectangle;
-
-	// Set the window's initial bounds
-	// Note that we're not guaranteed that the main monitor's lower left is at (0, 0)...
-	// We'll need to query for the global desktop bounds!
 	int left, bottom, right, top;
 	XPLMGetScreenBoundsGlobal(&left, &top, &right, &bottom);
 	params.left = left + 50;
@@ -65,9 +56,8 @@ PLUGIN_API int XPluginStart(
 
 	g_window = XPLMCreateWindowEx(&params);
 
-	// Position the window as a "free" floating window, which the user can drag around
 	XPLMSetWindowPositioningMode(g_window, xplm_WindowPositionFree, -1);
-	// Limit resizing our window: maintain a minimum width/height of 100 boxels and a max width/height of 300 boxels
+	// Min Width/Height Max Width/Height
 	XPLMSetWindowResizingLimits(g_window, 450, 75, 450, 75);
 	XPLMSetWindowTitle(g_window, "XP-RichPresence");
 
@@ -76,7 +66,6 @@ PLUGIN_API int XPluginStart(
 
 PLUGIN_API void	XPluginStop(void)
 {
-	// Since we created the window, we'll be good citizens and clean it up
 	XPLMDestroyWindow(g_window);
 	g_window = NULL;
 }
@@ -87,8 +76,7 @@ PLUGIN_API void XPluginReceiveMessage(XPLMPluginID inFrom, int inMsg, void* inPa
 
 void	draw_hello_world(XPLMWindowID in_window_id, void* in_refcon)
 {
-	// Mandatory: We *must* set the OpenGL state before drawing
-	// (we can't make any assumptions about it)
+	// Required: OpenGL State
 	XPLMSetGraphicsState(
 		0 /* no fog */,
 		0 /* 0 texture units */,
@@ -102,7 +90,7 @@ void	draw_hello_world(XPLMWindowID in_window_id, void* in_refcon)
 	int l, t, r, b;
 	XPLMGetWindowGeometry(in_window_id, &l, &t, &r, &b);
 
-	float col_white[] = { 1.0, 1.0, 1.0 }; // red, green, blue
+	float col_white[] = { 1.0, 1.0, 1.0 };
 
-	XPLMDrawString(col_white, l + 10, t - 20, "This version of the plugin does nothing so far. You can close this window.", NULL, xplmFont_Proportional);
+	XPLMDrawString(col_white, l + 10, t - 20, "This version of the plugin does nothing so far. You can close this window.", NULL, xplmFont_Proportional); // text warning of lack of functionality
 }
