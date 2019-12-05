35c3176f2f04f5e7c6bdd7a1397a926475924149
slimit75 <limitspeed75@gmail.com>
2019-12-04 19:37:53 -0500

    Add menu options (not functioning right now)

--- XP-RP.cpp
+++ XP-RP.cpp
@@ -3,6 +3,7 @@
 
 #include "XPLMDisplay.h"
 #include "XPLMGraphics.h"
+#include "XPLMMenus.h"
 #include <string.h>
 #if IBM
 #include <windows.h>
@@ -26,6 +27,10 @@ XPLMCursorStatus	dummy_cursor_status_handler(XPLMWindowID in_window_id, int x, i
 int					dummy_wheel_handler(XPLMWindowID in_window_id, int x, int y, int wheel, int clicks, void* in_refcon) { return 0; }
 void				dummy_key_handler(XPLMWindowID in_window_id, char key, XPLMKeyFlags flags, char virtual_key, void* in_refcon, int losing_focus) { }
 
+int g_menu_container_idx;
+XPLMMenuID g_menu_id;
+void menu_handler(void*, void*);
+
 PLUGIN_API int XPluginStart(
 	char* outName,
 	char* outSig,
@@ -35,6 +40,14 @@ PLUGIN_API int XPluginStart(
 	strcpy(outSig, "sl75.xp.richpresence");
 	strcpy(outDesc, "Discord Rich Presence for X-Plane 11.");
 
+	// Menu
+	g_menu_container_idx = XPLMAppendMenuItem(XPLMFindPluginsMenu(), "XP-RichPresence", 0, 0);
+	g_menu_id = XPLMCreateMenu("XP-RichPresence", XPLMFindPluginsMenu(), g_menu_container_idx, menu_handler, NULL);
+	XPLMAppendMenuItem(g_menu_id, "Re-Configure Flight", (void*)"Menu Item 1", 1);
+	XPLMAppendMenuSeparator(g_menu_id);
+	XPLMAppendMenuItem(g_menu_id, "Toggle Settings", (void*)"Menu Item 2", 1);
+	
+	// Inital Window
 	XPLMCreateWindow_t params;
 	params.structSize = sizeof(params);
 	params.visible = 1;
@@ -68,6 +81,8 @@ PLUGIN_API void	XPluginStop(void)
 {
 	XPLMDestroyWindow(g_window);
 	g_window = NULL;
+
+	XPLMDestroyMenu(g_menu_id);
 }
 
 PLUGIN_API void XPluginDisable(void) { }
@@ -93,4 +108,16 @@ void	draw_hello_world(XPLMWindowID in_window_id, void* in_refcon)
 	float col_white[] = { 1.0, 1.0, 1.0 };
 
 	XPLMDrawString(col_white, l + 10, t - 20, "This version of the plugin does nothing so far. You can close this window.", NULL, xplmFont_Proportional); // text warning of lack of functionality
+}
+
+void menu_handler(void* in_menu_ref, void* in_item_ref)
+{
+	if (!strcmp((const char*)in_item_ref, "Menu Item 1"))
+	{
+		return; //XPLMCommandOnce(XPLMFindCommand("sim/operation/toggle_settings_window"));
+	}
+	else if (!strcmp((const char*)in_item_ref, "Menu Item 2"))
+	{
+		return; //XPLMCommandOnce(XPLMFindCommand("sim/operation/toggle_key_shortcuts_window"));
+	}
 }
