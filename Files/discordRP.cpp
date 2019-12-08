573a63d9b0440039977e77645794683365e9cbf6
Speed_Limit75 <limitspeed75@gmail.com>
2019-12-08 14:31:15 -0500

    Update project settings

--- Files/discordRP.cpp
+++ Files/discordRP.cpp
@@ -1,10 +1,10 @@
 /*
-    ##########################
-    # XP-RP by Speed_Limit75 #
-    # discordRP.cpp/.h       #
-    ##########################
+	##########################
+	# XP-RP by Speed_Limit75 #
+	# discordRP.cpp/.h       #
+	##########################
 
-    This file manages the Discord Rich Presence.
+	This file manages the Discord Rich Presence.
 */
 
 #include "discordRP.h"
@@ -14,14 +14,14 @@
 discord::Core* core{};
 
 void initRP() {
-    auto result = discord::Core::Create(579668847846752266, DiscordCreateFlags_Default, &core);
-    discord::Activity activity{};
-    activity.SetState("Testing");
-    activity.SetDetails("Testing, but another line");
-    core->ActivityManager().UpdateActivity(activity, [](discord::Result result) {});
-    core->ActivityManager().RegisterSteam(1938123);
+	auto result = discord::Core::Create(579668847846752266, DiscordCreateFlags_Default, &core);
+	discord::Activity activity{};
+	activity.SetState("Testing");
+	activity.SetDetails("Testing, but another line");
+	core->ActivityManager().UpdateActivity(activity, [](discord::Result result) {});
+	core->ActivityManager().RegisterSteam(1938123);
 }
 
 void runCallback() {
-    ::core->RunCallbacks();
+	::core->RunCallbacks();
 }
