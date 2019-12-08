5a56cd3265208c02e1e6664d17d5aa270c43d27f
Speed_Limit75 <limitspeed75@gmail.com>
2019-12-08 10:32:16 -0500

    Create new files

---
+++ Files/discordRP.cpp
@@ -0,0 +1,8 @@
+/*
+	##########################
+	# XP-RP by Speed_Limit75 #
+	# discordRP.cpp/.h       #
+	##########################
+
+	This file manages the Discord Rich Presence.
+*/
---
+++ Files/discordRP.h
@@ -0,0 +1,9 @@
+// Header: See discordRP.cpp
+
+#pragma once
+
+/*
+bool stringToBool(char* text);
+bool readSettings(char* filePath, char* wanted);
+void writeSettings(char* filePath, bool settings[4]);
+*/
